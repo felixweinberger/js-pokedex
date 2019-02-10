3f948953fb3b7a8d65fe5250dcd74339afa4713c
Felix Weinberger <felix.weinberger@gmail.com>
2019-02-10 18:48:27 +0100

    completed model functions for grabbing pokemans

--- server/model.js
+++ server/model.js
@@ -5,22 +5,27 @@ const Pokemon = mongoose.model('Pokemon', {
   id: Number,
   name: String,
   sprite: String,
+  weight: Number,
+  baseExp: Number,
+  moves: [String],
+  abilities: [String],
+  types: [String],
 });
 
-mongoose.connect('mongodb://localhost/chat_server', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/pokemon', {useNewUrlParser: true});
 
+const model = {
+  getPokemon (id) {
+    return Pokemon.find({id: id});
+  },
+  getPokemons () {
+    return Pokemon.find({});
+  },
+};
 
+(async () => {
+  const mew = await model.getPokemon(151)[0];
+  console.log(mew);
+})();
 
-// const model = {
-//   getMessages () {
-//     return Message.find({});
-//   },
-//   addMessage (msg) {
-//     const newMsg = new Message({
-//       authorId: msg.authorId,
-//       content: msg.content,
-//       timeStamp: msg.timeStamp
-//     });
-//     return newMsg.save();
-//   }
-// };
+module.exports = model;
